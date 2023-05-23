d69f8b313d41ad1f8d1b72fa2157bccd8e12adea
karim-aboelazm <20912017100480@fci.zu.edu.eg>
2023-05-23 17:14:19 +0300

    Edit something

--- hr_salary/models/employee.py
+++ hr_salary/models/employee.py
@@ -4,7 +4,6 @@ from odoo import models, fields, api
 
 class HrSalary(models.Model):
     _inherit             = 'hr.employee'
-    name                 = fields.Char()
     employee_id          = fields.Char("Employee ID")
     employee_arabic_name = fields.Char()
       
